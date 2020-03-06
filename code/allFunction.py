5247fff254388487bf7e874b714ba6c3ea638702
Ziaee AmirAbbas <ziaeeamir@fh-kufstein.ac.at>
2020-03-06 21:17:30 +0100

    updated code

--- code/allFunction.py
+++ code/allFunction.py
@@ -13,9 +13,13 @@ import glob
 from sklearn import svm
 from sklearn.preprocessing import StandardScaler
 from skimage.feature import hog
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation  import train_test_split
+# But, if you are using scikit-learn >= 0.18 then use this:
+# from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 import pickle
+from scipy.ndimage.measurements import label
+
 
 
 # In[2]:
@@ -43,7 +47,7 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                                   cells_per_block=(cell_per_block, cell_per_block),
                                   block_norm= 'L2-Hys',
                                   transform_sqrt=False, 
-                                  visualise=vis, feature_vector=feature_vec)
+                                  feature_vector=feature_vec)
         return features, hog_image
     # Otherwise call with one output
     else:      
@@ -52,7 +56,7 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                        cells_per_block=(cell_per_block, cell_per_block),
                        block_norm= 'L2-Hys',
                        transform_sqrt=False, 
-                       visualise=vis, feature_vector=feature_vec)
+                       feature_vector=feature_vec)
         return features
 
 
@@ -266,6 +270,7 @@ def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
         else:
             hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
                         pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+            
         #8) Append features to list
         img_features.append(hog_features)
 
@@ -298,44 +303,45 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
 # and overlap fraction (for both x and y)
 def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None], 
                  xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    
  # If x and/or y start/stop positions not defined, set to image size
- if x_start_stop[0] == None:
-     x_start_stop[0] = 0
- if x_start_stop[1] == None:
-     x_start_stop[1] = img.shape[1]
- if y_start_stop[0] == None:
-     y_start_stop[0] = 0
- if y_start_stop[1] == None:
-     y_start_stop[1] = img.shape[0]
- # Compute the span of the region to be searched    
- xspan = x_start_stop[1] - x_start_stop[0]
- yspan = y_start_stop[1] - y_start_stop[0]
+    if x_start_stop[0] == None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] == None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] == None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] == None:
+        y_start_stop[1] = img.shape[0]
+  # Compute the span of the region to be searched    
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
  # Compute the number of pixels per step in x/y
- nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
- ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
  # Compute the number of windows in x/y
- nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
- ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
- nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step) 
- ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step) 
+    nx_buffer = np.int(xy_window[0]*(xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1]*(xy_overlap[1]))
+    nx_windows = np.int((xspan-nx_buffer)/nx_pix_per_step) 
+    ny_windows = np.int((yspan-ny_buffer)/ny_pix_per_step) 
  # Initialize a list to append window positions to
- window_list = []
+    window_list = []
  # Loop through finding x and y window positions
  # Note: you could vectorize this step, but in practice
  # you'll be considering windows one by one with your
  # classifier, so looping makes sense
- for ys in range(ny_windows):
-     for xs in range(nx_windows):
-         # Calculate window position
-         startx = xs*nx_pix_per_step + x_start_stop[0]
-         endx = startx + xy_window[0]
-         starty = ys*ny_pix_per_step + y_start_stop[0]
-         endy = starty + xy_window[1]
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs*nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys*ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
          
          # Append window position to list
-         window_list.append(((startx, starty), (endx, endy)))
- # Return the list of windows
- return window_list
+            window_list.append(((startx, starty), (endx, endy)))
+  # Return the list of windows
+    return window_list
 
 
 
@@ -394,4 +400,27 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
         features.append(np.concatenate(file_features))
     # Return list of feature vectors
     return features
+
+
+def draw_labeled_bbox(img,labels):
+    # Iterate through all detected cars
+    image=np.copy(img)
+    for car_number in range(1,labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero=(labels[0]==car_number).nonzero()
+        #identify x and y values of those pixels
+        nonzeroy=np.array(nonzero[0])
+        nonzerox=np.array(nonzero[1])
+        bbox=((np.min(nonzerox),np.min(nonzeroy)),(np.max(nonzerox),np.max(nonzeroy)))
+        cv2.rectangle(image,bbox[0],bbox[1],(0,0,255),6)
+    return image
+
+def applay_threshold(heatmap,threshold):
+    heatmap[heatmap<=threshold]=0
+    return heatmap
+
+def add_heat(heatmap,bbobx_list):
+    for bbox in bbobx_list:
+        heatmap[bbox[0][1]:bbox[1][1],bbox[0][0]:bbox[1][0]] +=1
+    return heatmap
 
